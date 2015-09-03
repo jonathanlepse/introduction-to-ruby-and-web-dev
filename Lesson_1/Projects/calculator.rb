e5d5c3ed08e74fede5660211eb73e499bf15098c
jonathanlepse <jonathan.lepselter@gmail.com>
2015-09-03 18:37:58 +0000

    removes if statement, cleans up elsif statement

--- Lesson_1/Projects/calculator.rb
+++ Lesson_1/Projects/calculator.rb
@@ -21,13 +21,7 @@ while true
   type_of_operation()
   command=gets.chomp
   
-  if command == 'exit'
-    puts "You have chosen to exit this program. Good-Bye."
-    break
-  end
-
-
-
+ 
   if command == "add"
     result = num1.to_i + num2.to_i
   elsif command == "subtract"
@@ -36,8 +30,10 @@ while true
     result = num1.to_i * num2.to_i
   elsif command == "divide"
     result= num1.to_f / num2.to_f
-  else
+  elsif command == "exit"
     result="You have chosen to end the program. Good-Bye"
+    puts result
+    break
   end
 
 puts "RESULT: #{result}"
