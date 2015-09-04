d46c5ebe4d308377ca682d55e4f8beacadb8383c
jonathanlepse <jonathan.lepselter@gmail.com>
2015-09-04 18:31:01 +0000

    Adds winning/tie message method

--- Lesson_1/Projects/rock_paper_scissors.rb
+++ Lesson_1/Projects/rock_paper_scissors.rb
@@ -2,6 +2,28 @@ puts "Welcome to the game!"
 
 CHOICES=["rock", "paper", "scissors"]
 
+def winning_message(winning_choice)
+  case winning_choice
+  when 'paper'
+    puts "Paper cover rock."
+  when 'rock'
+    puts "Rock smashes scissors"
+  when 'scissors'
+    puts "Scissors cuts paper"
+  end
+end
+
+def tie_message(winning_choice)
+  case winning_choice
+  when 'paper'
+    puts "You both chose paper."
+  when 'rock'
+    puts "You both chose rock."
+  when 'scissors'
+    puts "You both chose scissors"
+  end
+end
+  
 ask_again = " "
 while ask_again != 'no'
 
@@ -13,10 +35,13 @@ end until CHOICES.include?(player_choice)
 computer_choice=CHOICES.sample
 
 if player_choice == computer_choice
-  puts "Tie!"
+  tie_message(player_choice)
+  puts "It's a Tie!"
 elsif (player_choice == 'paper' && computer_choice == 'rock') || (player_choice=='rock' && computer_choice== 'scissors') ||(player_choice == 'scissors' && computer_choice=='paper')
+  winning_message(player_choice)
   puts "Congratulations! You win!"
 else
+  winning_message(computer_choice)
   puts "Bummer. Computer Wins."
 end
 
@@ -25,6 +50,7 @@ ask_again=gets.chomp
 if ask_again == 'yes'
   puts "Game is starting over...."
 else
-  puts "Good-Bye"
+  puts "Good-Bye!"
 end
+
 end
