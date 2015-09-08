fd693655f541ce96035df0ee1c9050fed2be8f93
jonathanlepse <jonathan.lepselter@gmail.com>
2015-09-08 17:06:46 +0000

    Adds inspections to check for proper user input

--- Lesson_1/Projects/calculator.rb
+++ Lesson_1/Projects/calculator.rb
@@ -11,34 +11,46 @@ end
 ask_again=" "
 while ask_again != 'no'
 
-enter_number
-num1=gets.chomp
+  enter_number
+  num1=gets.chomp
+  until num1 =~ /\d+/
+  puts "Please enter a valid number only."
+  num1=gets.chomp
+  end
   
-enter_number
-num2=gets.chomp
+  enter_number
+  num2=gets.chomp
+  until num2 =~ /\d+/
+  puts "Please enter a valid number only."
+  num2=gets.chomp
+  end
 
-type_of_operation
-command=gets.chomp
+  type_of_operation
+  command=gets.chomp
+  until command =~ /\w+/
+  puts "Please enter a valid command only."
+  command=gets.chomp
+  end
   
  
-if command == "add"
-  result = num1.to_f + num2.to_f
-elsif command == "subtract"
-  result = num1.to_f - num2.to_f
-elsif command == "multiply"
-  result = num1.to_f * num2.to_f
-elsif command == "divide"
-  result= num1.to_f / num2.to_f
-end
-
-puts "Your result is: #{result}"
-
-puts "Would you like to try another calculation? (yes/no)"
-ask_again=gets.chomp
-  if ask_again == 'yes'
-    puts "Begin Calculating...."
-  else 
-    puts "You have chosen to exit the program. Good-Bye"
+  if command == "add"
+    result = num1.to_f + num2.to_f
+  elsif command == "subtract"
+    result = num1.to_f - num2.to_f
+  elsif command == "multiply"
+    result = num1.to_f * num2.to_f
+  elsif command == "divide"
+    result= num1.to_f / num2.to_f
   end
+
+  puts "Your result is: #{result}"
+
+  puts "Would you like to try another calculation? (yes/no)"
+  ask_again=gets.chomp
+    if ask_again == 'yes'
+      puts "Begin Calculating...."
+    else 
+      puts "You have chosen to exit the program. Good-Bye"
+    end
 
 end
